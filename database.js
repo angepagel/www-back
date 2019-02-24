db9e5d8dc53d44bb9bc148a8493355a9f3721aad
Ange Pagel <inkandev@gmail.com>
2019-02-25 00:04:05 +0100

    Create /api/users endpoint

--- database.js
+++ database.js
@@ -2,13 +2,32 @@ const pgp = require('pg-promise')();
 const dotenv = require('dotenv').config();
 
 const config = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 }
 
 const database = pgp(config);
 
-module.exports = database;
+
+function getAllUsers(req, res, next) {
+  database.any('SELECT id, username FROM users')
+    .then(function(data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all users'
+        })
+    })
+    .catch(function(error) {
+        return next(error);
+    })
+}
+
+
+module.exports = {
+  getAllUsers: getAllUsers,
+};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const database = require('./database');
 const app = express();
 
 app.use(cors());
 
 app.listen(process.env.PORT);
+
+app.get('/api/users', database.getAllUsers);
 
 module.exports = app;
