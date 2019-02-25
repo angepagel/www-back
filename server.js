2bee7476e1f83e4ac216eddd123af152f346bea4
Ange Pagel <inkandev@gmail.com>
2019-02-25 16:21:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 const dotenv = require('dotenv').config();
 const database = require('./database');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(logger('dev'));
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
